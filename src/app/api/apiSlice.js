e713664f780269fc852e381123514f77b86232e8
Flashback <124055153+flashback265@users.noreply.github.com>
2026-08-22 12:21:50 +0100

    Delete src directory

--- src/app/api/apiSlice.js
+++
@@ -1,8 +0,0 @@
-import {creactApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react' 
-
-export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({baseUrl:'http://localhost:3500'}), 
-    tagTypes: [], 
-    endpoints:builder =>({})
-
-})
